db3670937d6941aa1d6bc3c3926b7e496f3bd360
BAJIUDOJI <validol.se@gmail.com>
2021-06-13 00:15:30 +0300

    new proj

---
+++ src/main/java/org/example/web_proj/domain/Role.java
@@ -0,0 +1,5 @@
+package org.example.web_proj.domain;
+
+public enum Role {
+    USER;
+}
---
+++ src/main/java/org/example/web_proj/repos/UserRepo.java
@@ -0,0 +1,8 @@
+package org.example.web_proj.repos;
+
+import org.example.web_proj.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
